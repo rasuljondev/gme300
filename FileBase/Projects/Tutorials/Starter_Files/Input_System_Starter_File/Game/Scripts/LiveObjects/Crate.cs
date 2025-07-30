6c93f1e60d884b85279f934fff1a776046f463da
Musokhon147 <musokhonjaloliddinov@gmail.com>
2025-07-30 08:52:11 +0500

    Forklift fixed

--- FileBase/Projects/Tutorials/Starter_Files/Input_System_Starter_File/Game/Scripts/LiveObjects/Crate.cs
+++ FileBase/Projects/Tutorials/Starter_Files/Input_System_Starter_File/Game/Scripts/LiveObjects/Crate.cs
@@ -2,32 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Interactions; // <-- Required for HoldInteraction
+using UnityEngine.InputSystem.Interactions;
 
 namespace Game.Scripts.LiveObjects
 {
     public class Crate : MonoBehaviour
     {
-        [SerializeField] private float _punchDelay;
+        [SerializeField] private float _punchDelay = 0.2f;
         [SerializeField] private GameObject _wholeCrate, _brokenCrate;
         [SerializeField] private Rigidbody[] _pieces;
         [SerializeField] private BoxCollider _crateCollider;
         [SerializeField] private InteractableZone _interactableZone;
 
+        [SerializeField] private int _requiredHitsToBreak = 5; // <-- how many punches required
+        private int _currentHits = 0; // <-- punch tracker
+
         private bool _isReadyToBreak = false;
         private List<Rigidbody> _brakeOff = new List<Rigidbody>();
-
         private PlayerInputActions _inputActions;
+        private bool _playerInZone = false;
 
         private void OnEnable()
         {
             InteractableZone.onZoneInteractionComplete += InteractableZone_onZoneInteractionComplete;
 
-            // Setup input system
             if (_inputActions == null)
             {
                 _inputActions = new PlayerInputActions();
-                _inputActions.Push.Punch.performed += OnPushPerformed;
+                _inputActions.Push.Punch.performed += OnPunchPerformed;
                 _inputActions.Enable();
             }
         }
@@ -38,7 +40,7 @@ namespace Game.Scripts.LiveObjects
 
             if (_inputActions != null)
             {
-                _inputActions.Push.Punch.performed -= OnPushPerformed;
+                _inputActions.Push.Punch.performed -= OnPunchPerformed;
                 _inputActions.Disable();
             }
         }
@@ -48,37 +50,41 @@ namespace Game.Scripts.LiveObjects
             _brakeOff.AddRange(_pieces);
         }
 
-        private void OnPushPerformed(InputAction.CallbackContext context)
+        private void OnPunchPerformed(InputAction.CallbackContext context)
         {
             if (!_isReadyToBreak || _interactableZone.GetZoneID() != 6)
                 return;
 
-            // Check if this is a hold interaction
             bool isHold = context.interaction is HoldInteraction;
 
             if (_brakeOff.Count > 0)
             {
                 BreakPart(isHold);
+                _currentHits++;
+
+                if (_currentHits >= _requiredHitsToBreak)
+                {
+                    _isReadyToBreak = false;
+                    _crateCollider.enabled = false;
+                    _interactableZone.CompleteTask(6);
+                    Debug.Log("Crate fully broken after punches!");
+                }
+
                 StartCoroutine(PunchDelay());
-            }
-            else
-            {
-                _isReadyToBreak = false;
-                _crateCollider.enabled = false;
-                _interactableZone.CompleteTask(6);
-                Debug.Log("Completely Busted");
             }
         }
 
         public void BreakPart(bool strong)
         {
             int rng = Random.Range(0, _brakeOff.Count);
-            _brakeOff[rng].constraints = RigidbodyConstraints.None;
+            Rigidbody part = _brakeOff[rng];
+
+            part.constraints = RigidbodyConstraints.None;
 
             Vector3 force = strong ? new Vector3(3f, 3f, 3f) : new Vector3(1f, 1f, 1f);
-            _brakeOff[rng].AddForce(force, ForceMode.Force);
+            part.AddForce(force, ForceMode.Force);
 
-            _brakeOff.Remove(_brakeOff[rng]);
+            _brakeOff.RemoveAt(rng);
         }
 
         IEnumerator PunchDelay()
@@ -95,14 +101,19 @@ namespace Game.Scripts.LiveObjects
 
         private void InteractableZone_onZoneInteractionComplete(InteractableZone zone)
         {
-            if (_isReadyToBreak == false && _brakeOff.Count > 0)
+            if (zone.GetZoneID() == 6)
             {
-                _wholeCrate.SetActive(false);
-                _brokenCrate.SetActive(true);
-                _isReadyToBreak = true;
-            }
+                _playerInZone = true;
 
-            // Action happens via new input now
+                // Now we wait for the punch input to start breaking
+                if (_isReadyToBreak == false && _brakeOff.Count > 0)
+                {
+                    _wholeCrate.SetActive(false);
+                    _brokenCrate.SetActive(true);
+                    _isReadyToBreak = true;
+                    Debug.Log("Crate activated, ready for punches.");
+                }
+            }
         }
 
         /*
@@ -116,7 +127,7 @@ namespace Game.Scripts.LiveObjects
                 _isReadyToBreak = true;
             }
 
-            if (_isReadyToBreak && zone.GetZoneID() == 6) //Crate zone
+            if (_isReadyToBreak && zone.GetZoneID() == 6)
             {
                 if (_brakeOff.Count > 0)
                 {
